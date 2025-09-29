60565946833d77e3fba3f52e32a9bbe5d2634cc7
DaniHDZM <sayu142184@gmail.com>
2025-09-28 19:42:44 -0600

    Url

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const repoName = 'Portafolio'; 
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/${repoName}/', 
+  base: 'https://danihdzm.github.io/Portafolio/', 
   plugins: [
     vue(),
     vueDevTools(),
